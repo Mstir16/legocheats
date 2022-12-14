c3173b1bcdef0439b743286ff8de770c0d5f6627
Mstir16 <72167139+Mstir16@users.noreply.github.com>
2022-12-13 21:50:02 -0500

    Update kysrespectfully.lua

--- archive/A Shura's Vision/kysrespectfully.lua
+++ archive/A Shura's Vision/kysrespectfully.lua
@@ -1167,3 +1167,8 @@ AutoStrikeForceFunc = function()
         end
     end
 end
+
+plr.Idled:connect(function()
+    game:GetService("VirtualUser"):CaptureController()
+    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
