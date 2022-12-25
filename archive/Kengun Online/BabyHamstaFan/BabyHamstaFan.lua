66141a2b4036a1f3f7129309910005a75d9bc308
Mstir16 <72167139+Mstir16@users.noreply.github.com>
2022-12-25 12:56:06 -0500

    Update BabyHamstaFan.lua

--- archive/Kengun Online/BabyHamstaFan/BabyHamstaFan.lua
+++ archive/Kengun Online/BabyHamstaFan/BabyHamstaFan.lua
@@ -1,17 +1,28 @@
 getgenv().isPermanent = false;
 getgenv().key = 'VzpFKKKTkPdwnnGD9hc17oF3NtlHLyfntkJbI0TWKh6Eqn1EdMjpJB2Az9FSPDmd'
 
+setreadonly(syn, false)
+
 --// did Hamstagang a favor <3 \\--
 local m1kestagang
 m1kestagang = hookfunction(game.HttpGet, function(LofiWas, here, ...)
     if here == "https://kylndantas-key-system.kylndantas.repl.co/verify" then
         return "f06baa562807eb1f7b735409a7ef2efb32b7a7d744c73bedd822f95fed19f480"
     elseif here == "https://raw.githubusercontent.com/KylnDantas/Valiant-UI/main/file.lua" then
-        here = "https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Bypassed_Scripts/KylnDantas_kengun-online-gui/Valiant-UI.lua"
+        here = "https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Kengun%20Online/BabyHamstaFan/Valiant-UI.lua"
         return m1kestagang(LofiWas, here, ...)
     end
     return m1kestagang(LofiWas, here, ...)
 end)
+
+local oldrequest = syn.request
+syn.request = function(t)
+    if t.Url == "https://raw.githubusercontent.com/KylnDantas/InstanceProtect/main/source.lua" then
+        t.Url = "https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Kengun%20Online/BabyHamstaFan/InstanceProtect.lua"
+	return oldrequest(t)
+    end
+    return oldrequest(t)
+end
 
 --Boronide™ Obfuscation, v0.6.4
 
