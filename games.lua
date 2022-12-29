79d6892822b3eaceac4275a48e5f11f3fd1a824a
Mstir16 <72167139+Mstir16@users.noreply.github.com>
2022-12-28 20:25:52 -0500

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,12 @@
+return {
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/A%20Shura's%20Vision/kysrespectfully.lua"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Allusions/script.lua"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Anime%20Adventures/Auto%20Erwin%20Buff%20Cycle.lua"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Aniverse/Autofarm.lua"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Blox%20Fruits/script.lua"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Deepwoken/CharismaAutoPaster.lua"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Football%20Fusion%202/mags.lua"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Greenville/Roadmap%20Dealership%20Autofarm"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Kengun%20Online/loafofbreadxm1kecorp"] = {ids},
+  ["https://raw.githubusercontent.com/Mstir16/legocheats/main/archive/Ohio./script.lua"] = {ids},
+}
