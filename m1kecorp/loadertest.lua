18858281f0d22b8720770d079c02dd9720d926c0
Mstir16 <72167139+Mstir16@users.noreply.github.com>
2023-03-14 05:20:07 -0400

    Update loadertest.lua

--- m1kecorp/loadertest.lua
+++ m1kecorp/loadertest.lua
@@ -1,1 +1,30 @@
+local http
+pcall(function()
+http = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
+end)
 
+if http == nil then return end
+
+local function GetGames()
+   local response = http({
+        Url = "https://api.github.com/repos/Mstir16/legocheats/contents/m1kecorp/games",
+        Method = "GET",
+    })
+
+    
+    local data = game.HttpService:JSONDecode(game.HttpService:JSONDecode(game.HttpService:JSONEncode(response.Body)))
+    return data
+end
+
+local GamesList = GetGames()
+local PID = game.PlaceId
+
+for i,v in pairs(GamesList) do
+    local script = v["download_url"]
+    local script_name = v["name"]
+    
+    if script_name:find(tostring(PID)) then
+        loadstring(game:HttpGet(script))()
+        break
+    end
+end
