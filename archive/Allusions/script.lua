c26875c2f3869a9b30bcf7830e86cfcbb3cac992
Mstir16 <72167139+Mstir16@users.noreply.github.com>
2023-03-25 21:54:45 -0400

    Update script.lua

--- archive/Allusions/script.lua
+++ archive/Allusions/script.lua
@@ -1,7 +1,9 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Allusions is shit <3", "Ocean")
-local Features = Window:NewTab("Tool's")
-local Allusions = Features:NewSection("m1kecorp 😪😪")
+local Features = Window:NewTab("Combat")
+local Farms = Window:NewTab("Autofarm")
+local Combat = Features:NewSection("m1kecorp 😪😪")
+local Farms = Farms:NewSection("Lofi on top <3")
 local vim = game:GetService("VirtualInputManager")
 local humroot = game.Players.LocalPlayer.Character.HumanoidRootPart
 
@@ -38,7 +40,60 @@ task.spawn(function()
     end
 end)
 
-Allusions:NewButton("RareTool Pickup", "RareTool Pickup", function(state)
+Combat:NewToggle("Inf Dozerify", "Inf Dozerify", function(state)
+    getgenv().TP = state
+    while getgenv().TP == true do
+        pcall(function()
+            if game.Players.LocalPlayer.Character:FindFirstChild("Dozerify") then
+                game.Players.LocalPlayer.Character:FindFirstChild("Dozerify"):Destroy()
+            end
+        end)
+    end
+end)
+
+Combat:NewToggle("Inf Russian Vitamins", "Inf Russian Vitamins", function(state)
+    getgenv().TP = state
+    while getgenv().TP == true do
+        pcall(function()
+            if game.Lighting:FindFirstChild("Atmosphere") then
+                game.Lighting.Atmosphere:Destroy()
+            end
+            
+            if game.Players.LocalPlayer.Character:FindFirstChild("RussianTripp") then
+                game.Players.LocalPlayer.Character.RussianTripp:Destroy()
+            end
+            
+            if game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("RussianVitamins") then
+                if game.Players.LocalPlayer.Character.HumanoidRootPart.RussianVitamins:FindFirstChild("HeartBeat") then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.RussianVitamins.HeartBeat:Destroy()
+                end           
+            end
+        end)
+    end
+end)
+
+Combat:NewToggle("Inf Candy Bag Buffs", "Inf Candy Bag Buffs", function(state)
+    getgenv().TP = state
+    while getgenv().TP == true do
+        pcall(function()
+            local lucky = {"Red","Blue","Yellow"}
+            
+            for i,v in pairs(lucky) do
+                if game.Players.LocalPlayer.Character:FindFirstChild(v) then
+                   v:Destroy() 
+                end
+            end
+        end)
+    end
+end)
+
+Combat:NewKeybind("Delete Selected Tool", "Certain tools that provide buffs can give inf buff",Enum.KeyCode.RightBracket, function()
+    pcall(function()
+        game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"):Destroy()
+    end)
+end)
+
+Farms:NewButton("RareTool Pickup", "RareTool Pickup", function(state)
     tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(1, Enum.EasingStyle.Linear)
     tween = tweenService:Create(game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(game:GetService("Workspace").Miscellaneous.RareTool.Position)})
     tween:Play()
@@ -50,7 +105,7 @@ Allusions:NewButton("RareTool Pickup", "RareTool Pickup", function(state)
     end
 end)
 
-Allusions:NewToggle("Auto Finance King (EQUIP)", "Auto Finance King (EQUIP)", function(state)
+Farms:NewToggle("Auto Finance King (EQUIP)", "Auto Finance King (EQUIP)", function(state)
     getgenv().TP = state
     
     local function equipPrimary()
@@ -92,7 +147,7 @@ Allusions:NewToggle("Auto Finance King (EQUIP)", "Auto Finance King (EQUIP)", fu
     end
 end)
 
-Allusions:NewToggle("Auto Minos Prime", "Auto Minos Prime", function(state)
+Farms:NewToggle("Auto Minos Prime", "Auto Minos Prime", function(state)
     getgenv().TP = state
     while getgenv().TP == true do
         pcall(function()
@@ -137,7 +192,7 @@ Allusions:NewToggle("Auto Minos Prime", "Auto Minos Prime", function(state)
     end
 end)
 
-Allusions:NewToggle("Auto Lord Cinder", "Auto Lord Cinder", function(state)
+Farms:NewToggle("Auto Lord Cinder", "Auto Lord Cinder", function(state)
     getgenv().TP = state
     while getgenv().TP == true do
         pcall(function()
@@ -154,7 +209,7 @@ Allusions:NewToggle("Auto Lord Cinder", "Auto Lord Cinder", function(state)
     end
 end)
 
-Allusions:NewToggle("Auto Black Silence", "Auto Black Silence", function(state)
+Farms:NewToggle("Auto Black Silence", "Auto Black Silence", function(state)
     getgenv().TP = state
     while getgenv().TP == true do
         pcall(function()
